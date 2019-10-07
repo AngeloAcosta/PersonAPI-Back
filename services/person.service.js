8d86be337f55f948f2b1448e8d6f7b25b7b5e9c1
Diego Marquina <dmarquina@belatrixsf.com>
2019-10-07 14:59:11 -0500

    Single Quoting fix

--- services/person.service.js
+++ services/person.service.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-const setupBaseService = require("./base.service");
+const setupBaseService = require('./base.service');
 
 module.exports = function setupPersonService(model) {
   let baseService = new setupBaseService();
@@ -10,12 +10,12 @@ module.exports = function setupPersonService(model) {
       const people = await model.findAll();
 
       baseService.returnData.responseCode = 200;
-      baseService.returnData.message = "Getting data successfully";
+      baseService.returnData.message = 'Getting data successfully';
       baseService.returnData.data = people;
     } catch (err) {
-      console.log("Error: ", err);
+      console.log('Error: ', err);
       baseService.returnData.responseCode = 500;
-      baseService.returnData.message = "" + err;
+      baseService.returnData.message = '' + err;
       baseService.returnData.data = [];
     }
 
@@ -33,47 +33,47 @@ module.exports = function setupPersonService(model) {
         //Proper data validation for each field to modify
         for (let prop in request.body) {
           if (
-            request.body[prop] === "" &&
-            prop !== "Contact" &&
-            prop !== "ContactType"
+            request.body[prop] === '' &&
+            prop !== 'Contact' &&
+            prop !== 'ContactType'
           ) {
             errors.push(`The field ${prop} is required.`);
           }
         }
 
         if (!/^[a-zA-ZñÑ'\s]{1,25}$/.test(request.body.name)) {
-          errors.push("Some characters in the Name field are not allowed.");
+          errors.push('Some characters in the Name field are not allowed.');
         }
 
         if (!/[a-zA-ZñÑ'\s]{1,25}/.test(request.body.lastName)) {
           errors.push(
-            "Some characters in the Last Name field are not allowed."
+            'Some characters in the Last Name field are not allowed.'
           );
         }
 
         if (!/[0-9]{4}-[0-9]{2}-[0-9]{2}/.test(request.body.birthdate)) {
-          errors.push("Invalid Birth Date field format.");
+          errors.push('Invalid Birth Date field format.');
         }
 
         if (!/^([0-9]){0,1}$/.test(request.body.documentTypeId)) {
-          errors.push("Invalid submitted Document Type value.");
+          errors.push('Invalid submitted Document Type value.');
         } else {
           switch (request.body.documentTypeId) {
-            case "1":
+            case '1':
               if (!/^[0-9]{8}$/.test(request.body.document)) {
-                errors.push("Invalid submitted DNI format.");
+                errors.push('Invalid submitted DNI format.');
               }
               break;
 
-            case "2":
+            case '2':
               if (!/^([a-zA-Z0-9]){12}$/.test(request.body.document)) {
-                errors.push("Invalid submitted PASSPORT format.");
+                errors.push('Invalid submitted PASSPORT format.');
               }
               break;
 
-            case "3":
+            case '3':
               if (!/^([a-zA-Z0-9]){12}$/.test(request.body.document)) {
-                errors.push("Invalid submitted CE format.");
+                errors.push('Invalid submitted CE format.');
               }
               break;
 
@@ -83,18 +83,18 @@ module.exports = function setupPersonService(model) {
         }
 
         if (!/^[0-9]{0,1}$/.test(request.body.genderId)) {
-          errors.push("Invalid submitted GenderId value.");
+          errors.push('Invalid submitted GenderId value.');
         }
 
         if (!/^[0-9]{0,2}$/.test(request.body.countryId)) {
-          errors.push("Invalid submitted CountryId value.");
+          errors.push('Invalid submitted CountryId value.');
         }
 
         if (
           !/^[0-9]{0,1}$/.test(request.body.contactTypeId1) ||
           !/^[0-9]{0,1}$/.test(request.body.contactTypeId2)
         ) {
-          errors.push("Contact Type field 1 or 2 are invalid");
+          errors.push('Contact Type field 1 or 2 are invalid');
         } else {
           //Hardcoded at the moment, need to be changed according to Front End team design
 
@@ -102,7 +102,7 @@ module.exports = function setupPersonService(model) {
           if (request.body.contactTypeId1 == 1) {
             //Telephone
             if (!/^([0-9]){6,9}$/.test(request.body.contact1)) {
-              errors.push("Invalid Telephone format.");
+              errors.push('Invalid Telephone format.');
             }
           } else if (request.body.contactTypeId1 == 2) {
             //Email
@@ -111,17 +111,17 @@ module.exports = function setupPersonService(model) {
                 request.body.contact1
               )
             ) {
-              errors.push("Invalid Email format.");
+              errors.push('Invalid Email format.');
             }
           } else {
-            errors.push("ContactTypeId field 1 is invalid."); //When is submitted other values like 3, 4 and so
+            errors.push('ContactTypeId field 1 is invalid.'); //When is submitted other values like 3, 4 and so
           }
 
           //Validation to Contac2
           if (request.body.contactTypeId2 == 1) {
             //Telephone
             if (!/^([0-9]){6,9}$/.test(request.body.contact2)) {
-              errors.push("Invalid Telephone format.");
+              errors.push('Invalid Telephone format.');
             }
           } else if (request.body.contactTypeId2 == 2) {
             //Email
@@ -130,10 +130,10 @@ module.exports = function setupPersonService(model) {
                 request.body.contact2
               )
             ) {
-              errors.push("Invalid Email format.");
+              errors.push('Invalid Email format.');
             }
           } else {
-            errors.push("ContactTypeId field 2 is invalid."); //When is submitted other values like 3, 4 and so
+            errors.push('ContactTypeId field 2 is invalid.'); //When is submitted other values like 3, 4 and so
           }
         }
 
@@ -141,7 +141,7 @@ module.exports = function setupPersonService(model) {
 
         if (errors.length) {
           baseService.returnData.responseCode = 400;
-          baseService.returnData.message = "Errors from data validation";
+          baseService.returnData.message = 'Errors from data validation';
           baseService.returnData.data = errors;
         } else {
           const personModified = await model.update(request.body, {
@@ -150,7 +150,7 @@ module.exports = function setupPersonService(model) {
 
           if (personModified) {
             baseService.returnData.responseCode = 200;
-            baseService.returnData.message = "Update completed successfully.";
+            baseService.returnData.message = 'Update completed successfully.';
             baseService.returnData.data = [];
           }
         }
@@ -161,9 +161,9 @@ module.exports = function setupPersonService(model) {
         baseService.returnData.data = errors;
       }
     } catch (err) {
-      console.log("Error: ", err);
+      console.log('Error: ', err);
       baseService.returnData.responseCode = 500;
-      baseService.returnData.message = "" + err;
+      baseService.returnData.message = '' + err;
       baseService.returnData.data = [];
     }
 
@@ -179,12 +179,12 @@ module.exports = function setupPersonService(model) {
       });
 
       baseService.returnData.responseCode = 200;
-      baseService.returnData.message = "Getting data successfully";
+      baseService.returnData.message = 'Getting data successfully';
       baseService.returnData.data = person;
     } catch (err) {
-      console.log("Error: ", err);
+      console.log('Error: ', err);
       baseService.returnData.responseCode = 500;
-      baseService.returnData.message = "" + err;
+      baseService.returnData.message = '' + err;
       baseService.returnData.data = [];
     }
 
