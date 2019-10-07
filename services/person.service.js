6f7f7938c4ce8b6119fb354aeadf0ebff989116a
Diego Marquina <dmarquina@belatrixsf.com>
2019-10-07 15:59:18 -0500

    Modularized

--- services/person.service.js
+++ services/person.service.js
@@ -22,12 +22,103 @@ module.exports = function setupPersonService(model) {
     return baseService.returnData;
   }
 
-  function checkBlankSpaces(data) {
+  function checkBlankSpacesforUpdate(data) {
+    let errors = [];
     for (let prop in data) {
       if (data[prop] === '' && prop !== 'Contact' && prop !== 'ContactType') {
         errors.push(`The field ${prop} is required.`);
       }
     }
+    return errors;
+  }
+
+  function checkNameFormatUpdate(data) {
+    let errors = [];
+    if (!/^[a-zA-ZñÑ'\s]{1,25}$/.test(data.name)) {
+      errors.push('Some characters in the Name field are not allowed.');
+    }
+
+    if (!/[a-zA-ZñÑ'\s]{1,25}/.test(data.lastName)) {
+      errors.push('Some characters in the Last Name field are not allowed.');
+    }
+    return errors;
+  }
+
+  function checkDocumentUpdate(data) {
+    let errors = [];
+    if (!/^([0-9]){0,1}$/.test(data.documentTypeId)) {
+      errors.push('Invalid submitted Document Type value.');
+    } else {
+      switch (data.documentTypeId) {
+        case '1':
+          if (!/^[0-9]{8}$/.test(data.document)) {
+            errors.push('Invalid submitted DNI format.');
+          }
+          break;
+
+        case '2':
+          if (!/^([a-zA-Z0-9]){12}$/.test(data.document)) {
+            errors.push('Invalid submitted PASSPORT format.');
+          }
+          break;
+
+        case '3':
+          if (!/^([a-zA-Z0-9]){12}$/.test(data.document)) {
+            errors.push('Invalid submitted CE format.');
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+    return errors;
+  }
+
+  function checkBirthDataUpdate(data) {
+    let errors = [];
+
+    if (!/[0-9]{4}-[0-9]{2}-[0-9]{2}/.test(data.birthdate)) {
+      errors.push('Invalid Birth Date field format.');
+    }
+
+    if (!/^[0-9]{0,1}$/.test(data.genderId)) {
+      errors.push('Invalid submitted GenderId value.');
+    }
+
+    if (!/^[0-9]{0,2}$/.test(data.countryId)) {
+      errors.push('Invalid submitted CountryId value.');
+    }
+    return errors;
+  }
+
+  function checkContactDataUpdate(dataTypeField, ContactValue) {
+    let errors = [];
+
+    if (!/^[0-9]{0,1}$/.test(dataTypeField)) {
+      errors.push('Contact Type field is invalid.');
+    } else {
+      //Validation to Contact1
+      if (dataTypeField == 1) {
+        //Telephone
+        if (!/^([0-9]){6,9}$/.test(request.body.contact1)) {
+          errors.push('Invalid Telephone format.');
+        }
+      } else if (dataTypeField == 2) {
+        //Email
+        if (
+          !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/.test(
+            ContactValue
+          )
+        ) {
+          errors.push('Invalid Email format.');
+        }
+      } else {
+        errors.push('Contact Type field is invalid.'); //When is submitted other values like 3, 4 and so
+      }
+    }
+
+    return errors;
   }
 
   async function modifyPerson(request) {
@@ -40,103 +131,27 @@ module.exports = function setupPersonService(model) {
       if (person) {
         //Proper data validation for each field to modify
 
-        checkBlankSpaces(request.body);
+        errors.concat(checkBlankSpacesforUpdate(request.body));
 
-        if (!/^[a-zA-ZñÑ'\s]{1,25}$/.test(request.body.name)) {
-          errors.push('Some characters in the Name field are not allowed.');
-        }
+        errors.concat(checkNameFormatUpdate(request.body));
 
-        if (!/[a-zA-ZñÑ'\s]{1,25}/.test(request.body.lastName)) {
-          errors.push(
-            'Some characters in the Last Name field are not allowed.'
-          );
-        }
+        errors.concat(checkDocumentUpdate(request.body));
 
-        if (!/[0-9]{4}-[0-9]{2}-[0-9]{2}/.test(request.body.birthdate)) {
-          errors.push('Invalid Birth Date field format.');
-        }
+        errors.concat(checkBirthDataUpdate(request.body));
 
-        if (!/^([0-9]){0,1}$/.test(request.body.documentTypeId)) {
-          errors.push('Invalid submitted Document Type value.');
-        } else {
-          switch (request.body.documentTypeId) {
-            case '1':
-              if (!/^[0-9]{8}$/.test(request.body.document)) {
-                errors.push('Invalid submitted DNI format.');
-              }
-              break;
+        errors.concat(
+          checkContactDataUpdate(
+            request.body.contactTypeId1,
+            request.body.contact1
+          )
+        );
 
-            case '2':
-              if (!/^([a-zA-Z0-9]){12}$/.test(request.body.document)) {
-                errors.push('Invalid submitted PASSPORT format.');
-              }
-              break;
-
-            case '3':
-              if (!/^([a-zA-Z0-9]){12}$/.test(request.body.document)) {
-                errors.push('Invalid submitted CE format.');
-              }
-              break;
-
-            default:
-              break;
-          }
-        }
-
-        if (!/^[0-9]{0,1}$/.test(request.body.genderId)) {
-          errors.push('Invalid submitted GenderId value.');
-        }
-
-        if (!/^[0-9]{0,2}$/.test(request.body.countryId)) {
-          errors.push('Invalid submitted CountryId value.');
-        }
-
-        if (
-          !/^[0-9]{0,1}$/.test(request.body.contactTypeId1) ||
-          !/^[0-9]{0,1}$/.test(request.body.contactTypeId2)
-        ) {
-          errors.push('Contact Type field 1 or 2 are invalid');
-        } else {
-          //Hardcoded at the moment, need to be changed according to Front End team design
-
-          //Validation to Contact1
-          if (request.body.contactTypeId1 == 1) {
-            //Telephone
-            if (!/^([0-9]){6,9}$/.test(request.body.contact1)) {
-              errors.push('Invalid Telephone format.');
-            }
-          } else if (request.body.contactTypeId1 == 2) {
-            //Email
-            if (
-              !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/.test(
-                request.body.contact1
-              )
-            ) {
-              errors.push('Invalid Email format.');
-            }
-          } else {
-            errors.push('ContactTypeId field 1 is invalid.'); //When is submitted other values like 3, 4 and so
-          }
-
-          //Validation to Contac2
-          if (request.body.contactTypeId2 == 1) {
-            //Telephone
-            if (!/^([0-9]){6,9}$/.test(request.body.contact2)) {
-              errors.push('Invalid Telephone format.');
-            }
-          } else if (request.body.contactTypeId2 == 2) {
-            //Email
-            if (
-              !/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(
-                request.body.contact2
-              )
-            ) {
-              errors.push('Invalid Email format.');
-            }
-          } else {
-            errors.push('ContactTypeId field 2 is invalid.'); //When is submitted other values like 3, 4 and so
-          }
-        }
+        errors.concat(
+          checkContactDataUpdate(
+            request.body.contactTypeId2,
+            request.body.contact2
+          )
+        );
 
         //Send Validation Errors or Update the data
 
