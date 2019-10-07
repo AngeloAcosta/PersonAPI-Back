99b256825f8d16638d76015b4028adccb086e470
Christian Manuel Changa Castillo <cchanga@belatrix.com>
2019-10-07 10:35:09 -0500

    Applied model corrections and seeders

---
+++ models/setup.seeders.js
@@ -0,0 +1,157 @@
+const faker = require('faker');
+
+async function seedContactTypes(model) {
+  await model.bulkCreate([
+    {
+      name: 'Teléfono',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Correo electrónico',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    }
+  ]);
+}
+
+async function seedCountries(model) {
+  await model.bulkCreate([
+    {
+      name: 'Perú',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Ecuador',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Colombia',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Venezuela',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Guyana',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Surinam',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Brazil',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Paraguay',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Uruguay',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Argentina',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Chile',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    }
+  ]);
+}
+
+async function seedDocumentTypes(model) {
+  await model.bulkCreate([
+    {
+      name: 'DNI',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Pasaporte',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Carnet de extranjeria',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    }
+  ]);
+}
+
+async function seedGenders(model) {
+  await model.bulkCreate([
+    {
+      name: 'Masculino',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Femenino',
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    }
+  ]);
+}
+
+async function seedPeople(model) {
+  let countryIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  let documentTypeIds = [1, 2, 3];
+  let genderIds = [1, 2];
+  let contactTypeIds = [1, 2];
+  let people = [];
+  for (let index = 0; index < 25; index++) {
+    let genderId = faker.random.arrayElement(genderIds);
+    let gender = '';
+    if (genderId == 1) {
+      gender = 'male';
+    } else {
+      gender = 'female';
+    }
+    let contactTypeId = faker.random.arrayElement(contactTypeIds);
+    let contact = '';
+    if (contactTypeId == 1) {
+      contact = faker.phone.phoneNumber();
+    } else {
+      contact = faker.internet.email();
+    }
+    people.push({
+      name: faker.name.firstName(gender),
+      lastName: faker.name.lastName(),
+      birthdate: faker.date.past(),
+      document: faker.random.alphaNumeric(8).toUpperCase(),
+      documentTypeId: faker.random.arrayElement(documentTypeIds),
+      genderId,
+      countryId: faker.random.arrayElement(countryIds),
+      contact,
+      contactTypeId,
+      createdAt: faker.date.past(),
+      updatedAt: new Date()
+    });
+  }
+  await model.bulkCreate(people);
+}
+
+module.exports = {
+  seedContactTypes,
+  seedCountries,
+  seedDocumentTypes,
+  seedGenders,
+  seedPeople
+}
