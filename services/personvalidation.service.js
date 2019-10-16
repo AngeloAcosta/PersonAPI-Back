22b0a2dbdd77b748085d36bf86a9f203fe2ece88
Bertha Teresa Peña Rivera <bpena@belatrix.com>
2019-10-16 12:01:32 -0500

    fix minor things

--- services/personvalidation.service.js
+++ services/personvalidation.service.js
@@ -1,21 +1,22 @@
 'use strict';
+
 module.exports = function personValidationSetup(){
     function checkNameFormat(data) {
-    let errors = [];
-    if (data.name) {
-        if (!/^[a-zA-ZñÑ'\s]{1,25}$/.test(data.name)) {
-          errors.push('Some characters in the Name field are not allowed.');
+      let errors = [];
+      if (data.name) {
+          if (!/^[a-zA-ZñÑ'\s]{1,25}$/.test(data.name)) {
+            errors.push('Some characters in the Name field are not allowed.');
+          }
         }
-      }
-  
-      if (data.lastName) {
-        if (!/[a-zA-ZñÑ'\s]{1,25}/.test(data.lastName)) {
-          errors.push('Some characters in the Last Name field are not allowed.');
+    
+        if (data.lastName) {
+          if (!/[a-zA-ZñÑ'\s]{1,25}/.test(data.lastName)) {
+            errors.push('Some characters in the Last Name field are not allowed.');
+          }
         }
+    
+        return errors;
       }
-  
-      return errors;
-    }
 
   function checkDocument(data) {
     let errors = [];
@@ -82,18 +83,18 @@ module.exports = function personValidationSetup(){
   function checkContactData(dataTypeField, contactValue) {
     let errors = [];
     if (dataTypeField && contactValue) {
-      if (dataTypeField != '' && contactValue != '') {
+      if(!dataTypeField && !contactValue) {
         // If the dataTypeField is blank
         if (!/^[0-9]{0,1}$/.test(dataTypeField)) {
           errors.push('Contact Type field is invalid.');
         } else {
           //Validation to Contact1
-          if (dataTypeField == 1) {
+          if (dataTypeField === 1) {
             //Telephone
             if (!/^([0-9]){6,9}$/.test(contactValue)) {
               errors.push('Invalid Telephone format.');
             }
-          } else if (dataTypeField == 2) {
+          } else if (dataTypeField === 2) {
             //Email
             if (!/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(contactValue)) {
               errors.push('Invalid Email format.');
@@ -109,11 +110,11 @@ module.exports = function personValidationSetup(){
   }
 
   function isInValidPassport(documentTypeId, document) {
-    return documentTypeId == 2 && document.length > 12;
+    return documentTypeId === 2 && document.length > 12;
   }
 
   function isNoForeignValidCard(documentTypeId, document) {
-    return documentTypeId == 3 && document.length > 12;
+    return documentTypeId === 3 && document.length > 12;
   }
 
   function checkBlankSpacesfor(data) {
