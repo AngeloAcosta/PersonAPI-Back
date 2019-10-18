e4f9f8b7bd6dabe00b9ec11bd5c84a1ad6945f81
Bertha Teresa Peña Rivera <bpena@belatrix.com>
2019-10-18 11:12:33 -0500

    starting

---
+++ services/kinshiplogic.service.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function setupKinshipLogic(){
+  
+    function IsGrandpa(person,relative){
+            if(person.id && relative.id){
+               if(person.kinshipType === "F"){
+                    if()
+               } 
+            }
+
+    }
+    return {
+        IsGrandpa
+    }
+}
