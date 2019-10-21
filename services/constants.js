8eb233d31b8026172ac7908af0d2423528ff51e9
DiegoMod <dmonteagudo@belatrixsf.com>
2019-10-21 08:41:54 -0500

    constants

--- services/constants.js
+++ services/constants.js
@@ -2,8 +2,6 @@ module.exports = {
     fatherKinshipType: 'F',
     motherKinshipType: 'M',
     siblingKinshipType: 'B',
-    grandfatherKinshipType: 'GF',
-    grandmotherKinshipType: 'GM',
     grandmothermotherkinshipTyp: 'GMM',
     grandfathermotherkinshipTyp: 'GFM',
     grandmotherfatherkinshipTyp: 'GMF',
