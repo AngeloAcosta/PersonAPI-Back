21994309e5cb64ea7bd5cb87785565b205df2fed
Christian Manuel Changa Castillo <cchanga@belatrix.com>
2019-10-22 13:38:50 -0500

    Fixes

--- services/authentication.service.js
+++ services/authentication.service.js
@@ -6,23 +6,23 @@ module.exports = function setupAuthenticationService() {
   const baseService = new setupBaseService();
 
   async function login(data) {
-    return baseService.responseData();
+    return baseService.getServiceResponse(200, 'Success', {});
   }
 
   async function checkLogin(email, password) {
-    return baseService.responseData();
+    return baseService.getServiceResponse(200, 'Success', {});
   }
 
   async function changePassword(password) {
-    return baseService.responseData();
+    return baseService.getServiceResponse(200, 'Success', {});
   }
 
   async function resetPassword(email) {
-    return baseService.responseData();
+    return baseService.getServiceResponse(200, 'Success', {});
   }
 
   async function logout() {
-    return baseService.responseData();
+    return baseService.getServiceResponse(200, 'Success', {});
   }
   
   return {
--- services/user.service.js
+++ services/user.service.js
@@ -3,36 +3,28 @@
 const setupBaseService = require('./base.service');
 
 module.exports = function setupUserService(userModel) {
-  const model = userModel;
-
   let baseService = new setupBaseService();
 
   async function create(userData) {
-    baseService.responseData(200,'Getting data successfully');
-    return baseService.returnData;
+    return baseService.getServiceResponse(200, 'Success', {});
   }
 
   async function doList() {
     try {
       const users = await userModel.findAll();
-      baseService.responseData(200,'Getting data successfully',users);
+      return baseService.getServiceResponse(200, 'Success', users);
     } catch (err) {
       console.log('Error: ', err);
-      baseService.responseData(500,err,[]);
+      return baseService.getServiceResponse(500, 'Error', {});
     }
-
-    return baseService.returnData;
   }
 
   async function findById(id) {
-    baseService.responseData(200,'Getting data successfully');
-    return baseService.returnData;
+    return baseService.getServiceResponse(200, 'Success', {});
   }
 
   async function update(userId, userData) {
-    baseService.responseData(200,'Getting data successfully',[]);
-
-    return baseService.returnData;
+    return baseService.getServiceResponse(200, 'Success', {});
   }
 
   return {
