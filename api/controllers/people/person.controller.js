106c39864c74fb63c2efbd662e8a888dfa1d3aa9
Christian <christianempire@hotmail.com>
2019-11-05 02:01:36 -0500

    Implemented endpoint for retrieving person tree

--- api/controllers/people/person.controller.js
+++ api/controllers/people/person.controller.js
@@ -88,6 +88,27 @@ const getKinships = async (request, response) => {
   return response.status(responseCode).json(responseData);
 }
 
+const getKinshipsTree = async (request, response) => {
+  let responseCode;
+  let responseData;
+  try {
+    // Inject services
+    const sharedService = await serviceContainer('shared');
+    // Get the person id from the route
+    const personId = request.params.id;
+    // Get the person kinships tree
+    const personData = await sharedService.buildPersonKinshipsTree(personId);
+    // Return the data
+    responseCode = personData.responseCode;
+    responseData = baseController.getSuccessResponse(personData.data, personData.message);
+  } catch (err) {
+    console.error('Error: ', err);
+    responseCode = 500;
+    responseData = baseController.getErrorResponse('Error');
+  }
+  return response.status(responseCode).json(responseData);
+}
+
 const post = async (request, response) => {
   let responseCode;
   let responseData;
@@ -212,6 +233,7 @@ module.exports = {
   doDelete,
   get,
   getKinships,
+  getKinshipsTree,
   post,
   postKinships,
   postKinshipsTest,
